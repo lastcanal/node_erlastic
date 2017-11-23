3eba07bec9ddd5b73234166b8e1775791d797e11
David Simon <davidsimon@swellinvesting.com>
2017-11-23 15:07:57 -0800

    Add parens to self calls in test.exs

--- test/test.exs
+++ test/test.exs
@@ -3,7 +3,7 @@ defmodule Exos.Proc do
 
   def init({cmd,init,opts}) do
     port = Port.open({:spawn,'#{cmd}'}, [:binary,:exit_status, packet: 4] ++ opts)
-    send(port,{self,{:command,:erlang.term_to_binary(init)}})
+    send(port,{self(),{:command,:erlang.term_to_binary(init)}})
     {:ok,port}
   end
 
@@ -12,12 +12,12 @@ defmodule Exos.Proc do
   def handle_info(_,port), do: {:noreply,port}
 
   def handle_cast(term,port) do
-    send(port,{self,{:command,:erlang.term_to_binary(term)}})
+    send(port,{self(),{:command,:erlang.term_to_binary(term)}})
     {:noreply,port}
   end
 
   def handle_call(term,_reply_to,port) do
-    send(port,{self,{:command,:erlang.term_to_binary(term)}})
+    send(port,{self(),{:command,:erlang.term_to_binary(term)}})
     res = receive do {^port,{:data,b}}->:erlang.binary_to_term(b) end
     {:reply,res,port}
   end
